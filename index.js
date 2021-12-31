3101220747981657ffbf3dbf4b937f256c8f3460
unknown <dasilvadeoliveirafabio@gmail.com>
2021-12-31 13:00:13 -0200

    teste de deploy 13

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
-const rota = require('./rotas')
+//const rota = require('./rotas')
 const PORT = process.env.PORT || 4000
-app.use(rota)
-
+//app.use(rota)
+app.get('/',(req,res)=>{
+    res.send('teste')
+})
 app.use(express.static('imagens'))
 app.listen(PORT)
