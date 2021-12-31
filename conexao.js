a67aeaf4e7a0749377cc4bff5a28fa124c383baf
unknown <dasilvadeoliveirafabio@gmail.com>
2021-12-31 11:07:32 -0200

    teste de deploy 7

--- conexao.js
+++ conexao.js
@@ -1,9 +1,11 @@
 const Sequelize = require('sequelize');
 require('dotenv').config()
+
 const sequelize = new Sequelize(process.env.BANCO,  process.env.USUARIO,process.env.SENHA,{
     host:"sql10.freesqldatabase.com",
     dialect:'mysql'
   })
+  
   sequelize.authenticate().then(function(){
       console.log('conectado com sucesso')
   }).catch(function(erro){
