0054e494998b8ef075101377bf98ff9792e26510
unknown <dasilvadeoliveirafabio@gmail.com>
2021-12-31 13:02:39 -0200

    teste de deploy 14

--- conexao.js
+++ conexao.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
 module.exports = ()=>{
     
-    const mongoose = require('mongoose')
-    mongoose.connect(process.env.CREDENCIAIS,{
-        useNewUrlParser:true
+    const mongoose = require('mongoose');
+    mongoose.connect('mongodb+srv://meuBanco:fabio2020@cluster0.w2xad.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+      useNewUrlParser:true,
+      useUnifiedTopology:true
     })
     const db = mongoose.connection
     db.on("erro",error=>console.error(error))
